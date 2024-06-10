3ed3377c20a468773f458eb580fd3dd92b8a19cf
keshavsoft <nknnkumar@yahoo.com>
2024-06-11 00:27:54 +0530

    keshav

--- src/js/pages/items/clickFunc.js
+++ src/js/pages/items/clickFunc.js
@@ -1,6 +1,54 @@
-let StartFunc = async () => {
+let StartFunc1 = async () => {
     console.log("aaaaaaaaa");
 
 };
 
-export { StartFunc }
+let StartFunc = async () => {
+    var $table = $('#table');
+    let jVarLocalData = await readImage();
+
+    $table.bootstrapTable({ data: jVarLocalData });
+};
+
+const readImage = () => {
+    return new Promise((resolve, reject) => {
+        var file = document.getElementById("myfile").files[0];
+
+        if (file) {
+            var reader = new FileReader();
+            reader.readAsText(file, "text/xml");
+            let ReturnArray = [];
+
+            reader.onload = function (evt) {
+                const parser = new DOMParser();
+                const doc = parser.parseFromString(evt.target.result.replaceAll("&#4;", ""), "application/xml");
+
+                let checkboxes = doc.documentElement.querySelectorAll("STOCKITEM");
+
+                checkboxes.forEach((userItem) => {
+                    // console.log("aaaaaaaaaaaaa : ", userItem, userItem.querySelector("OPENINGRATE"));
+
+                    let LoopInsideObject = {};
+                    LoopInsideObject.ItemName = userItem.attributes.NAME.value;
+
+                    let LoopInsideRate = userItem.querySelector("OPENINGRATE");
+                    // console.log("aaaaaaaaaaaaa : ", userItem, userItem.querySelector("OPENINGRATE"));
+
+                    if (LoopInsideRate === null === false) {
+                        console.log("aaaaaaaaaaaaa : ", LoopInsideRate, LoopInsideRate.innerHTML);
+
+                        LoopInsideObject.OpeningRate = LoopInsideRate.innerHTML;
+                    };
+
+                    ReturnArray.push(LoopInsideObject);
+                });
+
+                resolve(ReturnArray);
+            }
+            reader.onerror = function (evt) {
+            };
+        };
+    });
+};
+
+export { StartFunc };
